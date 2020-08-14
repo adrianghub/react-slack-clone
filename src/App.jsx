2862f2ce8b0b8137739084a3f7774e5629b73f0d
Adrian <adrian_zinko@op.pl>
2020-08-14 21:49:06 +0200

    feat: setup src init files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="app">
+      <h1>React Slack Clone</h1>
+
+      {/* Header */}
+      {/* Sidebar */}
+      {/* React-Router Chat */}
+    </div>
+  );
+};
+
+export default App;
